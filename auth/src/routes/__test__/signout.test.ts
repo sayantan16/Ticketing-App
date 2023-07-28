f321499d2d99ac16848081014cc2bdab22eb1848
sayantan16 <sayantankundu1611@gmail.com>
2023-07-28 15:51:52 -0400

    fixing the auth test cases for build failure

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -1,22 +1,14 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('It clears the cookies after signing out', async () => {
+it('clears the cookie after signing out', async () => {
   await request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test7@test.com',
+      email: 'test@test.com',
       password: 'password',
     })
     .expect(201);
-
-  await request(app)
-    .post('/api/users/signin')
-    .send({
-      email: 'test7@test.com',
-      password: 'password',
-    })
-    .expect(200);
 
   const response = await request(app)
     .post('/api/users/signout')
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('Returns a 201 on successful signup', async () => {
+it('returns a 201 on successful signup', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
@@ -11,35 +11,47 @@ it('Returns a 201 on successful signup', async () => {
     .expect(201);
 });
 
-it('It returns a 400 with an invalid email', async () => {
+it('returns a 400 with an invalid email', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test@test',
+      email: 'alskdflaskjfd',
       password: 'password',
     })
     .expect(400);
 });
 
-it('It returns a 400 with an invalid password', async () => {
+it('returns a 400 with an invalid password', async () => {
   return request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test1@test.com',
+      email: 'alskdflaskjfd',
       password: 'p',
     })
     .expect(400);
 });
 
-it('It returns a 400 with missing email & password', async () => {
-  return request(app).post('/api/users/signup').send({}).expect(400);
-});
-
-it('It disallows duplicate emails', async () => {
+it('returns a 400 with missing email and password', async () => {
   await request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test2@test.com',
+      email: 'test@test.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'alskjdf',
+    })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
       password: 'password',
     })
     .expect(201);
@@ -47,19 +59,17 @@ it('It disallows duplicate emails', async () => {
   await request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test2@test.com',
+      email: 'test@test.com',
       password: 'password',
     })
     .expect(400);
 });
 
-// cookies are set when requested over https request thats why
-// app.ts has secure: true under cookie session object
-it('It sets a cookie after successful signup', async () => {
+it('sets a cookie after successful signup', async () => {
   const response = await request(app)
     .post('/api/users/signup')
     .send({
-      email: 'test3@test.com',
+      email: 'test@test.com',
       password: 'password',
     })
     .expect(201);
